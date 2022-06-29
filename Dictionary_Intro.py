db23dad5c2a255bb841d197ecbf30fcd255b4add
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-29 19:03:54 +0530

    Add files via upload

---
+++ Dictionary_Intro.py
@@ -0,0 +1,11 @@
+'''Dictionary is the combination of key value pair'''
+# a={}
+# print(a)
+
+# a={1:'python',
+# 2:'java',
+# 'n':'core python'}
+# print(a[1])  #python
+# print(a[2])  #java
+# print(a['n'])  #core python
+# print(a[5])  #key error
